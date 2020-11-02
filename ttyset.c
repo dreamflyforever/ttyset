e764af2468b1d7bd8ec363a3a1484ad184f58b93
Jim <sjyangv0@gmail.com>
2020-11-02 11:08:09 +0800

    fix the compile bug
    Signed-off-by: Jim <sjyangv0@gmail.com>

--- ttyset.c
+++ ttyset.c
@@ -11,8 +11,8 @@ int main(int argc, char* argv[])
 {
 
 	if (argc != 3) {
-		printf("%s device speed\n\nSet speed for a serial device.\n
-			For instance:\n    %s /dev/ttyUSB0 75000\n", argv[0], argv[0]);                   
+		printf("%s device speed\n\nSet speed for a serial device.\n"
+			"For instance:\n    %s /dev/ttyUSB0 75000\n", argv[0], argv[0]);                   
 		return -1;
 	}
 
