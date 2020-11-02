3aa5fc38ee61e223d2a195bb104e1f481cbf8571
Jim <sjyangv0@gmail.com>
2020-11-02 10:26:57 +0800

    code indent
    Signed-off-by: Jim <sjyangv0@gmail.com>

--- ttyset.c
+++ ttyset.c
@@ -7,29 +7,31 @@
 #include <errno.h>
 #include <asm/termios.h>
 
-int main(int argc, char* argv[]) {
+int main(int argc, char* argv[])
+{
 
-    if (argc != 3) {
-        printf("%s device speed\n\nSet speed for a serial device.\nFor instance:\n    %s /dev/ttyUSB0 75000\n", argv[0], argv[0]);                   
-        return -1;
-    }
+	if (argc != 3) {
+		printf("%s device speed\n\nSet speed for a serial device.\n
+			For instance:\n    %s /dev/ttyUSB0 75000\n", argv[0], argv[0]);                   
+		return -1;
+	}
 
-    int fd = open(argv[1], O_RDONLY);
+	int fd = open(argv[1], O_RDONLY);
 
-    int speed = atoi(argv[2]);
+	int speed = atoi(argv[2]);
 
-    struct termios2 tio;
-    ioctl(fd, TCGETS2, &tio);
-    tio.c_cflag &= ~CBAUD;
-    tio.c_cflag |= BOTHER;
-    tio.c_ispeed = speed;
-    tio.c_ospeed = speed;
-    int r = ioctl(fd, TCSETS2, &tio);
-    close(fd);
+	struct termios2 tio;
+	ioctl(fd, TCGETS2, &tio);
+	tio.c_cflag &= ~CBAUD;
+	tio.c_cflag |= BOTHER;
+	tio.c_ispeed = speed;
+	tio.c_ospeed = speed;
+	int r = ioctl(fd, TCSETS2, &tio);
+	close(fd);
 
-    if (r == 0) {
-        printf("Changed successfully.\n");
-    } else {
-        perror("ioctl");
-    }
+	if (r == 0) {
+		printf("Changed successfully.\n");
+	} else {
+		perror("ioctl");
+	}
 }
